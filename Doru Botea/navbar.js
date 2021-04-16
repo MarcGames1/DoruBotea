43dedbfd0ff61dc4621c2703e66adc06445fef5e
marcgames1 <alex@DESKTOP-VHVLO28.localdomain>
2021-04-16 16:38:31 +0300

    4

---
+++ Doru Botea/navbar.js
@@ -0,0 +1,7 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    let x = document.querySelector(".bot-header")
+   
+    x.classList.toggle('hide')
+    
+  }
