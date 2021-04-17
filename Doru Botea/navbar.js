465d5d6839dcb2b473043b2f4e79629e9596d217
marcgames1 <alex@DESKTOP-VHVLO28.localdomain>
2021-04-17 14:19:39 +0300

    fixed navbar responsive bar

--- Doru Botea/navbar.js
+++ Doru Botea/navbar.js
@@ -1,7 +1,23 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    let x = document.querySelector(".bot-header")
-   
-    x.classList.toggle('hide')
-    
+    let x = document.querySelectorAll(".bot-header  nav ul  li")
+   x.forEach(li => li.classList.toggle('responsive'))
+}
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+    
+    
