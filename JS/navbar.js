0c4ff7d504cf947069dacc95fe1e2403a70cfb35
marcgames1 <alex@DESKTOP-VHVLO28.localdomain>
2021-05-05 11:39:15 +0300

    recover svg

--- JS/navbar.js
+++ JS/navbar.js
@@ -5,14 +5,18 @@ function myFunction() {
 }
 
 
-var docWidth = document.documentElement.offsetWidth;
+let slideIndex = 0;
+showSlides();
 
-[].forEach.call(
-  document.querySelectorAll('*'),
-  function(el) {
-    if (el.offsetWidth > docWidth) {
-      console.log(el);
-    }
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-);
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
     
