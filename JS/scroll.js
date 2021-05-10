27178d0d4837aeac128e5830bae05a3ccc68ea96
marcgames1 <alex@DESKTOP-VHVLO28.localdomain>
2021-05-10 17:06:25 +0300

    finished typografy

---
+++ JS/scroll.js
@@ -0,0 +1,17 @@
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+const scrollToServicii = ()=>{scroolTo("#servicii")}
+
+
+function scroolTo(selector){
+    var e = document.querySelector(selector);
+e.scrollIntoView();
+}
