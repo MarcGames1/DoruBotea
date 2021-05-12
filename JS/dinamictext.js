8787e3338c819cc3c02528ab3d4cf063a4e301ec
marcgames1 <alex@DESKTOP-VHVLO28.localdomain>
2021-05-12 14:20:14 +0300

    added google bots

---
+++ JS/dinamictext.js
@@ -0,0 +1,7 @@
+const textContent = [" drept penal", " dreptul familiei", " drept civil",  " malpraxis medical", " dreptul muncii", " executari silite" ]
+let index = 0
+function changeText(){
+    (index < textContent.length-1) ? index++ : index=0;
+    document.querySelector(".dinamic").innerText= textContent[index]
+}
+setInterval(changeText, 3000);
--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,9 @@
-const textContent = [" drept penal", " dreptul familiei", " drept civil",  " malpraxis medical", " dreptul muncii", " executari silite" ]
 const whatsapp = document.querySelector('.whatsapp-cta')
 const upArrow= document.querySelector(".buttonTop")
 const sections = document.querySelectorAll("section")
 
-let index = 0
-function changeText(){
-    (index < textContent.length-1) ? index++ : index=0;
-    document.querySelector(".dinamic").innerText= textContent[index]
-}
 
-setInterval(changeText, 3000);
+
 setTimeout(addButtons, 5000)
 
 
